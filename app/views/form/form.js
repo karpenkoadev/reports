6c5884ba41cebb948521bd3f131d2e00ac105388
MrCroissant <vladislav.korzh95@gmail.com>
2016-01-28 01:37:17 +0200

    post reports in firebase

--- app/views/form/form.js
+++ app/views/form/form.js
@@ -9,18 +9,65 @@ angular.module('myApp.formMod', ['ngRoute', 'firebase'])
   });
 }])
 
-.controller('formCtrl', ["$scope", '$firebaseAuth', function($scope, $firebaseAuth) {
+.controller('formCtrl', ["$scope", '$firebaseAuth', '$firebaseArray', function($scope, $firebaseAuth, $firebaseArray) {
     var ref = new Firebase("https://reports1.firebaseio.com");
     var auth = $firebaseAuth(ref);
     var getAuth = auth.$getAuth(); // auth user info
-    console.log()
+    $scope.nameUser = 'Jhon';
+    $scope.AddReprot = function() {
+        var addFrom = $firebaseArray(ref);
 
-    // init date picker
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
+        var dates = $scope.reports1.datePik;
+        var countBild = $scope.reports1.countBild;
+        var profiles = $scope.reports1.profiles;
+        var linkOnBid = $scope.reports1.linkOnBid;
+        var comments = $scope.reports1.comments;
+
+        if (dates === "undefined") {
+            dates = 'Не заполненно'
+        } else if (countBild === "undefined") {
+            countBild = 'Не заполненно'
+        } else if (profiles === "undefined") {
+            profiles = 'Не заполненно'
+        } else if (linkOnBid === "undefined") {
+            linkOnBid = 'Не заполненно'
+        } else if (comments === "undefined") {
+            comments = 'Не заполненно'
+        }
+
+
+
+    console.log(dates + countBild + profiles + linkOnBid + comments)
+
+
+
+
+
+
+
+        addFrom.$add({
+            dates: dates,
+            countBild: countBild,
+            profiles: profiles,
+            linkOnBid: linkOnBid,
+            comments: comments
+        }).then(function(ref) {
+            console.log(ref);
+        }, function(error) {
+            console.log("Error:", error);
+        });
+
+    }
+
+
+
+
     $(document).ready(function() {
+    // init date picker
+        $('.datepicker').pickadate({
+            selectMonths: true, 
+            selectYears: 15 
+        });
         $('select').material_select();
     });
 }]);
