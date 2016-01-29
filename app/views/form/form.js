5cd524319b170494de9cd890f7356f895b29748f
vladislav_korzh <vladislav.korzh95@gmail.com>
2016-01-29 18:12:25 +0200

    get user info

--- app/views/form/form.js
+++ app/views/form/form.js
@@ -10,47 +10,67 @@ angular.module('myApp.formMod', ['ngRoute', 'firebase'])
 }])
 
 .controller('formCtrl', ["$scope", '$firebaseAuth', '$firebaseArray', function($scope, $firebaseAuth, $firebaseArray) {
-    var ref = new Firebase("https://reports1.firebaseio.com/reports");
+    var userArr = [];
+    var userObj = {};
+    var ref = new Firebase("https://reports1.firebaseio.com/");
+    var refAddReports = ref.child('reports');
+    var refUsers = ref/*.child('users')*/;
     var auth = $firebaseAuth(ref);
+    var usersList= $firebaseArray(refUsers);
     var getAuth = auth.$getAuth(); // auth user info
+    console.log(getAuth)
     $scope.nameUser = 'Jhon';
     $scope.events = {
         selectItem : function($event){
             console.log($event, $scope.reports1);
         }
-    }
-    $scope.AddReprot = function() {
-        var addFrom = $firebaseArray(ref);
+    };
 
+    usersList.$loaded()
+        .then(function() {
+            angular.forEach(usersList, function (usersList) {
+                console.log(usersList.users);
+            });
+            var email = usersList['2670ae88-d7cc-464a-b233-42514fade42c'];
+            console.log(email)
+            /*for (var i = 0; i < usersList.length; i++) {
+                userObj[i] = usersList[i]
+            }*/
+        });
+
+
+    $scope.AddReprot = function() {
+        var addFrom = $firebaseArray(refAddReports);
+
+        var nameBider = "Jhon Dou";
         var dates = $scope.reports1.datePik;
         var countBild = $scope.reports1.countBild;
         var profiles = $scope.reports1.profiles;
         var linkOnBid = $scope.reports1.linkOnBid;
         var comments = $scope.reports1.comments;
-    console.log(dates + countBild + profiles + linkOnBid + comments);
-
-
-        setInterval(function(){
-            console.log($scope.reports1);
-        }, 1500);
-
 
 
 
         addFrom.$add({
+            nameBider: nameBider,
             dates: dates,
             countBild: countBild,
             profiles: profiles,
             linkOnBid: linkOnBid,
             comments: comments
-        }).then(function(ref) {
-            console.log(ref);
+        }).then(function(refAddReports) {
+            console.log(refAddReports);
         }, function(error) {
             console.log("Error:", error);
         });
 
     };
 
+
+
+
+
+    // init materialize plugin
     $(document).ready(function() {
     // init date picker
         $('.datepicker').pickadate({
@@ -58,5 +78,6 @@ angular.module('myApp.formMod', ['ngRoute', 'firebase'])
             selectYears: 15 
         });
         $('select').material_select();
+        
     });
 }]);
